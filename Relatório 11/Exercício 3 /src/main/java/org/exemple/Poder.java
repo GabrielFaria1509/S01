3c24ee5311e52f55321ae2e7a9c897954a8ec906
GabrielFaria1509 <fariagabriel1509@gmail.com>
2025-11-02 21:12:44 -0300

    Update Poder.java

--- Relatório 11/Exercício 3 /src/main/java/org/exemple/Poder.java
+++ Relatório 11/Exercício 3 /src/main/java/org/exemple/Poder.java
@@ -1,21 +1,16 @@
 // --- Arquivo: Poder.java ---
-
-/**
- * Classe que representa o poder de uma entidade.
- */
+package org.example;
 public class Poder {
-    // CORREÇÃO: O UML indica '+' (public) para 'nome'.
+    // + (public) como no diagrama
     public String nome;
 
-    // Construtor pedido no UML
+    // + (public) como no diagrama
     public Poder(String nome) {
         this.nome = nome;
-    
-    // já que 'nome' é público.
+    }
 
     @Override
     public String toString() {
-        // Acesso direto ao atributo público.
         return this.nome;
     }
 }
