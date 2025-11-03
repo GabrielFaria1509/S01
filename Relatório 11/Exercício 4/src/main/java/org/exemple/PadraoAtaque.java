5d31cb803633e95bc4d32005cb291d16f2f44a22
GabrielFaria1509 <fariagabriel1509@gmail.com>
2025-11-02 21:37:41 -0300

    Remove toString method from PadrãoAtaque class
    Removed the toString method for better clarity.

---
+++ Relatório 11/Exercício 4/src/main/java/org/exemple/PadraoAtaque.java
@@ -0,0 +1,18 @@
+// --- Arquivo: PadrãoAtaque.java ---
+
+/**
+ * Classe que representa um ataque.
+
+ */
+package.org.exemple
+public class PadrãoAtaque {
+    // + (public) como no diagrama
+    public String nome;
+    public int dano;
+
+    // + (public) construtor
+    public PadrãoAtaque(String nome, int dano) {
+        this.nome = nome;
+        this.dano = dano;
+    }
+}
